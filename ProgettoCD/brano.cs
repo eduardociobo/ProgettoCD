a399a516875f8d2f130a5101d531527dcd92a78e
Francesco Serrani <FRANCESCO.SERRANI@local.ittcesena.it>
2022-11-05 09:21:39 +0100

    brano classe aggiunto

---
+++ ProgettoCD/brano.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoCD
+{
+    internal class brano
+    {
+        string titolo, autore;
+        double durata;
+
+        public brano(string titolobrano, double duratabrano)
+        {
+            titolo = titolobrano;
+            durata = duratabrano;
+        }
+
+        public string getTitolo()
+        {
+            return titolo;
+        }
+
+        public string getAutore()
+        {
+            return autore;
+        }
+
+        public double getDurata()
+        {
+            return durata;
+        }
+
+        public void setTitolo(string titolodacambiare)
+        {
+            titolo = titolodacambiare;
+        }
+
+        public void setAutore(string autoredacambiare)
+        {
+            autore = autoredacambiare;
+        }
+
+        public void setDurata(string duratadacambiare)
+        {
+            durata = Convert.ToDouble(duratadacambiare);
+        }
+
+        public string toString()
+        {
+            return "L'autore del brano " + titolo + " è: " + autore + " e la durata è di " + durata + " minuti.";
+        }
+
+        public bool shortSong(double tempo)
+        {
+            if(durata < tempo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
